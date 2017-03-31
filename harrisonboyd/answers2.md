449fbc5a7549abd04819af02559e19643cd8ab4a
Harrison Boyd <harrisonboyd630@gmail.com>
2017-03-31 10:01:27 -0400

    harrison hw

---
+++ harrisonboyd/answers2.md
@@ -0,0 +1,42 @@
+1.Select the string expressions from the listed options.
+
+    a. "Hello World"
+    b. "Your number is " + someNumber
+    c. "Hello" + "World"
+    d. true && false
+
+
+answer: a and c
+
+2.Select all the falsey values from the following options:
+
+    a. false
+    b. 0
+    c. ""
+    d. -5
+    e. null
+
+answer: a, b, c and e
+
+3. Define the term "strict equality".
+
+answer: strict equality is defined by ===, if values are two different types they are considered unequal E.G. null === undefined
+
+4.What does ((42 - 8) + 24 / 6 - 3 * 0) * (34 + (7 + (4 + 3) % 3) / 2) evaluate to? Please evaluate this by hand.
+
+    a. 1444
+    b. 49
+    c. 1425
+    d. 712
+
+answer: a
+
+5. Which of the following operators do I use to invert a logical expression?
+    a. ?
+    b. -
+    c. !
+    d. ^
+
+answer: c
+
+
---
+++ harrisonboyd/javascript-exercises2.js
@@ -0,0 +1,150 @@
+//1. List the five falsey values in JavaScript.
+/*
+false
+0
+""
+null
+undefined
+NaN
+*/
+
+//2. List five truthy values in JavaScript.
+/*
+true
+[]
+{}
+"harrison"
+13
+*/
+
+//3.Take a look at the code example below and state what the variable sameAge will be set to and explain why.
+var myAge = 33;
+var yourAge = "33";
+
+var sameAge = (myAge === yourAge) ? "Yes!" : "No!";
+
+//answer: it would be set too the string "no!" because myAge is a number and yourAge is a string so they can not be striclly equal
+
+//4.Write a small program that has a line of code that does each of the following and prints out the result:
+
+//Add two numbers together.
+//EG: eight plus fifteen
+var num1 = 8;
+var num2 = 15;
+
+var sum = num1 + num2;
+
+console.log(sum);
+
+//Add three numbers together using the addition operator.
+//EG: eight plus fifteen plus negative one hundred
+var num1 = 8;
+var num2 = 9;
+var num3 = 10;
+
+var sum = num1 + num2 + num3;
+
+console.log(sum);
+
+//Add a positive and a negative number.
+//EG: negative five plus thirty two point eight
+var num1 = -8;
+var num2 = 8;
+
+var sum = num1 + num2;
+
+console.log(sum);
+
+//Subtract two numbers.
+//EG: thirty five minus two point eight
+var num1 = 8;
+var num2 = 7;
+
+var diff = num1 - num2;
+
+console.log(diff);
+
+//Subtract three numbers.
+//EG: thirty five minus two point eight minus seventeen
+var num1 = 8;
+var num2 = 9;
+var num3 = 10;
+
+var diff = num1 - num2 - num3;
+
+console.log(diff);
+
+//Subtract a positive and a negative number.
+//EG: negative five plus thirty two minus eight
+var num1 = 8;
+var num2 = -8;
+
+var diff = num1 - num2;
+
+console.log(diff);
+
+//Combine the addition and subtraction operators with three numbers.
+//EG: seventy five plus negative thirty five minus two point eight
+var num1 = 8;
+var num2 = 9;
+var num3 = 10;
+
+var sumdiff = num1 - num2 + num3;
+
+console.log(sumdiff);
+
+//Multiply two numbers
+//EG: six times negative zero point one
+var num1 = 8;
+var num2 = 9;
+
+var multi = num1 * num2;
+
+console.log(multi);
+
+//Multiply three numbers where one is negative
+//EG: negative eight times five times one billion point eight
+var num1 = 8;
+var num2 = -8;
+var num3 = 9;
+
+var multi = num1 * num2 * num3;
+
+console.log(multi);
+
+//Divide two numbers
+//EG: five divided by negative twelve.
+var num1 = 8;
+var num2 = 16;
+
+var divid = num1 / num2;
+
+console.log(divid);
+
+//Divide three numbers
+//EG: twenty five divided by negative twelve divided by two
+var num1 = 8;
+var num2 = 16;
+var num3 = 4;
+
+var divid = num1 / num2 / num3;
+
+console.log(divid);
+
+//Find the remainder of dividing two integers
+// EG: The remainder left when dividing twenty five by four
+var num1 = 8;
+var num2 = 17;
+
+var mod = num1 % num2;
+
+console.log(mod);
+
+
+// An expression that uses each of the following: Negative and Positive Numbers, Integers and Doubles, Parenthesis, Addition, Subtraction, Multiplication, Division, and Modulo.
+// EG: The remainder of five plus negative four minus negative four times three divided by two point three, divided by nine
+
+
+
+
+
